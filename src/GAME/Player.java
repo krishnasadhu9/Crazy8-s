455d776dbc003fafbbe4e5a27e03d24d7abf5adf
krishna sadhu <krishnasadhu9@gmail.com>
2019-04-22 16:38:26 -0400

    player

--- src/GAME/Player.java
+++ src/GAME/Player.java
@@ -2,4 +2,28 @@ package GAME;
 
 public class Player {
 
+		  private String name;
+		  private CardCollection hand;
+
+		  public Player(String name) {
+		    this.name = name;
+		    this.hand = new CardCollection(name);
+		  }
+
+		  
+		  public String getName() {
+		    return name;
+		  }
+
+		  
+		  public CardCollection getHand() {
+		    return hand;
+		  }
+
+
+		 
+		  public void display() {
+		    hand.display();
+		  }
+
 }
