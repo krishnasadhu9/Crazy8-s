23179faba306aa4ef1ff39afc6c0b37cb20b17b9
krishna sadhu <krishnasadhu9@gmail.com>
2019-04-22 16:35:50 -0400

    sprint1

--- src/GAME/Card.java
+++ src/GAME/Card.java
@@ -1,5 +1,34 @@
 package GAME;
 
+import java.util.*;
+
 public class Card {
+	
+	public static String[] Rank = {
+			
+			"Ace" , "2" , "3" , "4" , "5" , "6" , "7" , "8" ,"9", "10",
+			"Jack", "Queen", "King"
+	};
+	
+	public static String[] Suit = { "Clubs", "Diamonds", "Hearts", "Spades"};
+	
+	private int rank;
+	private int suit;
+	
+	public Card(int rank, int suit)
+	{
+		this.rank = rank;
+		this.suit = suit;	
+	}
+	
+	public String PrintCard()
+	{
+		return "The card - " + Rank[rank] + Suit[suit];
+	}
+	
+	
+	
+	
+	
 
 }
--- src/GAME/CardCollection.java
+++ src/GAME/CardCollection.java
@@ -1,5 +1,36 @@
 package GAME;
 
+import java.util.*;
+
 public class CardCollection {
 
+	 private String label;
+	 private ArrayList<Card> cards;
+	
+	public CardCollection(String label)
+	{
+		this.label = label;
+	}
+	
+	 public void addCard(Card card)
+	 {
+		    cards.add(card);
+     }
+	 
+	 public Card removeCard(int i)
+	 {
+		 return cards.remove(i);
+	 }
+	 
+	 public int size() {
+		    return cards.size();
+		  }
+	
+	 public void display() {
+		    System.out.println(label + ": ");
+		    for (Card card: cards) {
+		      System.out.println(card);
+		    }
+		    System.out.println();
+		  }
 }
